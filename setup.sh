f72d48c85fa9aaf8a6d26745c6c520d6e34fc298
Kryštof Kořínek <chriskorinek@seznam.cz>
2022-06-13 10:53:43 +0200

    Fix ZSH paths

--- setup.sh
+++ setup.sh
@@ -6,4 +6,8 @@ sudo apt install -y vim git curl zsh python httpie
 cp .vimrc ~/.vimrc
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp .zshrc ~/.zshrc
+echo "export ZSH=\"$(eval echo ~$USER)\"/.oh-my-zsh" > ~/.zshrc \
+    && cat .zshrc >> ~/.zshrc
+chsh -s $(which zsh)
+
+zsh
